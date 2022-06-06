69a45aa41a50fa9041d0272fd87d359c1b80f256
Wesley Souza <wfsouza23@gmail.com>
2022-06-06 14:38:05 -0300

    Service layer, component registration

--- src/main/java/com/wesleysouza/course/repositories/UserRepository.java
+++ src/main/java/com/wesleysouza/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.wesleysouza.course.repositories;
 import com.wesleysouza.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
---
+++ src/main/java/com/wesleysouza/course/resources/UserResource.java
@@ -0,0 +1,34 @@
+package com.wesleysouza.course.resources;
+
+import com.wesleysouza.course.entities.User;
+import com.wesleysouza.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+}
