1624e729831a8d2f31175e87bfd7396fd2dabc6c
Wesley Souza <wfsouza23@gmail.com>
2022-06-06 23:19:29 -0300

    Product - part2

---
+++ src/main/java/com/wesleysouza/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.wesleysouza.course.services;
+
+import com.wesleysouza.course.entities.Product;
+import com.wesleysouza.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
