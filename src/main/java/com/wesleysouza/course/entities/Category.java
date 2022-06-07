cba995748971f960e63a9ba5ba1b0ead72fac724
Wesley Souza <wfsouza23@gmail.com>
2022-06-06 23:17:27 -0300

    Product

--- src/main/java/com/wesleysouza/course/entities/Category.java
+++ src/main/java/com/wesleysouza/course/entities/Category.java
@@ -2,7 +2,9 @@ package com.wesleysouza.course.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +15,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -36,6 +41,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/wesleysouza/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.wesleysouza.course.repositories;
+
+import com.wesleysouza.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
