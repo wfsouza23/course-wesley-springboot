6456494c62b18e62607afd348f39682acf0797a8
Wesley Souza <wfsouza23@gmail.com>
2022-06-12 21:08:42 -0300

    Exception handling - findById

--- src/main/java/com/wesleysouza/course/services/UserService.java
+++ src/main/java/com/wesleysouza/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.wesleysouza.course.services;
 
 import com.wesleysouza.course.entities.User;
 import com.wesleysouza.course.repositories.UserRepository;
+import com.wesleysouza.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
